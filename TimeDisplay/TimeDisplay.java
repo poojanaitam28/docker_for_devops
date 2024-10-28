cc31afbf26b2c50866c7356cada8c61bb6f62278
Ubuntu <ubuntu@ip-172-31-2-126.ap-south-1.compute.internal>
2024-10-28 07:05:30 +0000

    added TimeDisplay Folder

---
+++ TimeDisplay/TimeDisplay.java
@@ -0,0 +1,10 @@
+// TimeDisplay.java
+
+import java.time.LocalTime;
+
+public class TimeDisplay {
+    public static void main(String[] args) {
+        System.out.println("Current time: " + LocalTime.now());
+    }
+}
+
